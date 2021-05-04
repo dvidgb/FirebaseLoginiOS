a41ce3be09dee1e56bbed587d264add22c1cdcce
David Bueno <davidbuenodbc@gmail.com>
2021-05-04 15:27:14 +0200

    Creado proyecto, cajas de texto y botones con estilo

---
+++ FirebaseTutorial/AuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AuthViewController.swift
+//  FirebaseTutorial
+//
+//  Created by David Bueno on 4/5/21.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
